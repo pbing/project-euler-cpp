54278d09477649dcba0a56be2173265a029a118c
Bernd Beuster <bernd.beuster@gmail.com>
2025-12-07 22:51:26 +0100

    Add SECTION

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,12 +4,14 @@
 #include "problem001.hpp"
 #include "problem002.hpp"
 
-TEST_CASE("Fibonacci") {
-  REQUIRE(fibonacci(0) == 0ULL);
-  REQUIRE(fibonacci(1) == 1ULL);
-  REQUIRE(fibonacci(2) == 1ULL);
-  REQUIRE(fibonacci(3) == 2ULL);
-  REQUIRE(fibonacci(10) == fibonacci(8) + fibonacci(9));
+TEST_CASE("Utils") {
+  SECTION("fibonacci") {
+    REQUIRE(fibonacci(0) == 0ULL);
+    REQUIRE(fibonacci(1) == 1ULL);
+    REQUIRE(fibonacci(2) == 1ULL);
+    REQUIRE(fibonacci(3) == 2ULL);
+    REQUIRE(fibonacci(10) == fibonacci(8) + fibonacci(9));
+  }
 }
 
 TEST_CASE("Problems") {
