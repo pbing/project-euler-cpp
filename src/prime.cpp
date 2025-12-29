36e1c3abfbd2871f037993f8917a349d448d4eb6
Bernd Beuster <bernd.beuster@gmail.com>
2025-12-29 21:46:54 +0100

    Remove benchmark statistics

--- src/prime.cpp
+++ src/prime.cpp
@@ -1,16 +1,5 @@
-#include <print>
-
 #include "prime.hpp"
 
-/*
-benchmark name                       samples       iterations    est run time
-                                     mean          low mean      high mean
-                                     std dev       low std dev   high std dev
--------------------------------------------------------------------------------
-isPrime(0x10001)                               100           211     1.6458 ms
-                                        78.5358 ns    78.3267 ns    78.8301 ns
-                                        1.25446 ns   0.964017 ns    1.70229 ns
-*/
 bool isPrime(unsigned n) {
   if (n < 2)
     return false;
